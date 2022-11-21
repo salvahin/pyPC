3b6de051f862f4d2d15aeafc19ad17586789709f
Miguel Sanchez <mike@Miguels-MacBook-Pro.local>
2022-11-21 13:19:41 -0600

    multi variable in program

--- BD_AL_test/minimum.py
+++ BD_AL_test/minimum.py
@@ -1,5 +1,5 @@
-def min(x,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,u,v):
-    a = [x,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,u,v]
+def min(*var):
+    a = list(var)
     # smallest number
     smallest = a[0]
     
