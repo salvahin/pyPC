6412106aedb27c4de88ad39c245b2a5d5ceae5ef
Miguel Sanchez <mike@Miguels-MacBook-Pro.local>
2023-03-17 13:16:55 -0600

    new structure for pymoo use

--- BD_AL_test/run_fitness.py
+++ BD_AL_test/run_fitness.py
@@ -6,13 +6,62 @@ import ast
 import numpy as np
 from tree_converter import TreeVisitor
 from test_fitness import Fitness
+from pymoo.util.display.output import Output
+from pymoo.util.display.column import Column
+from pymoo.visualization.scatter import Scatter
+from pymoo.core.problem import Problem
+from pymoo.algorithms.soo.nonconvex.pso import PSO
+from pymoo.algorithms.soo.nonconvex.ga import GA
+from pymoo.algorithms.soo.nonconvex.de import DE
+from pymoo.algorithms.soo.nonconvex.nelder import NelderMead
+from pymoo.algorithms.soo.nonconvex.pattern import PatternSearch
+from pymoo.algorithms.soo.nonconvex.cmaes import CMAES
+from pymoo.algorithms.soo.nonconvex.sres import SRES
+from pymoo.algorithms.soo.nonconvex.isres import ISRES
+from pymoo.operators.sampling.lhs import LHS
+from pymoo.optimize import minimize
 
-def run_algorithm(algorithm):
+paths = {
+    "test_programs/test_game_programs/function_only_testings/rock_paper_scissor_player_choice.py": 1,
+    "test_programs/test_game_programs/function_only_testings/bounce_draw.py": 2,
+    "test_programs/test_game_programs/function_only_testings/guess_the_number_input_guess.py": 1,
+    "test_programs/test_game_programs/function_only_testings/jogo_da_velha_python_actualizar_jogadas.py": 1,
+    "test_programs/test_game_programs/function_only_testings/rock_paper_scissor_number_to_name.py": 1,
+    "test_programs/test_game_programs/function_only_testings/TRPG_character_create_character.py": 1
+}
+
+algorithms = [PSO(pop_size=100, w= 0.7, c1=2, c2=2),
+              GA(pop_size=100, eliminate_duplicates=True),
+              DE(
+                 pop_size=100,
+                 sampling=LHS(),
+                 variant="DE/rand/1/bin",
+                 CR=0.3,
+                 dither="vector",
+                 jitter=False
+                ),
+              NelderMead(),
+              PatternSearch(),
+              SRES(n_offsprings=200, rule=1.0 / 7.0, gamma=0.85, alpha=0.2),
+              ISRES(n_offsprings=200, rule=1.0 / 7.0, gamma=0.85, alpha=0.2)]
+# algorithm = CMAES(x0=np.random.random(dimensions))  # Only for more than 1 dimension
+
+
+
+def run_algorithm(algorithm, problem=None):
     """
     Function that runs the selected algorithm into the tree nodes
     """
-    cost, pos = algorithm.optimize(fitness.fitness_function, iters=100)
-    return cost, pos
+    try:
+        cost, pos = algorithm.optimize(fitness.fitness_function, iters=100)
+    except:
+        result = minimize(
+                          problem, 
+                          algorithm, 
+                          ('n_iter', 100),
+                          
+                          verbose=True)
+    return result.F, result.X
 
 def convert_tree(path):
     """
@@ -28,6 +77,32 @@ def convert_tree(path):
     print(visitor.nodes)
     return visitor
 
+class MyOutput(Output):
+
+    def __init__(self):
+        super().__init__()
+        self.x_cost = Column("x_mean", width=13)
+        self.x_std = Column("x_std", width=13)
+        self.columns += [self.x_mean, self.x_std]
+
+    def update(self, algorithm):
+        super().update(algorithm)
+        self.x_mean.set(np.mean(algorithm.pop.get("X")))
+        self.x_std.set(np.std(algorithm.pop.get("X")))
+
+class FitnessProblem(Problem):
+
+    def __init__(self, fitness=None, dimensions=None):
+        xl = -999999 # longest number in an int
+
+        xu = 999999 # longest number in an int
+        self.fitness = fitness
+        self.ndim = dimensions
+        super().__init__(n_var=dimensions, n_obj=1, xl=xl, xu=xu)
+
+    def _evaluate(self, x, out, *args, **kwargs):
+        out["F"] = self.fitness.fitness_function(x)
+
 
 if __name__ == '__main__':
     st = time.time()
@@ -39,13 +114,16 @@ if __name__ == '__main__':
     max_paths = 0
     coverage = 0
     past_walking = []
-    options = {'c1': 2, 'c2': 2, 'w': 0.7}
-    particles = 100
+    #options = {'c1': 2, 'c2': 2, 'w': 0.7}
+    # particles = 100
     dimensions = 1
-    gbpso = GBPSO(particles,dimensions,options=options)
+    problem = FitnessProblem(fitness, dimensions)
+    algorithm = algorithms[2]
+    # algorithm = CMAES(x0=np.random.random(dimensions))  # Only for more than 1 dimension
+    # gbpso = GBPSO(particles,dimensions,options=options)
     while more_paths:
-        
-        cost, pos = run_algorithm(gbpso)
+        #cost, pos = run_algorithm(gbpso)
+        cost, pos = run_algorithm(algorithm, problem)
         particle_pos = np.array([pos],np.float32)
         fitness.resolve_path(particle_pos)
         has_path = lambda x: lambda y: y in x
